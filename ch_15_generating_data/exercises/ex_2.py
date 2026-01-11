1022137874cd2323668ac4699165e1ab76299c08
LockStockandalotofcode <shivanie0101@gmail.com>
2026-01-12 00:43:23 +0530

    feat:feat: Data Visualization project ch 15 ex 2: Colored Cubes
    Add colormap visualization to cubes plot using matplotlib color gradients

---
+++ ch_15_generating_data/exercises/ex_2.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+x_values = range(1, 5001)
+y_values = [x**3 for x in x_values]
+
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, c=y_values, cmap='magma', s=10)
+# ax.plot(x_values, y_values, linewidth=10)
+
+# set chart title and label axes.
+ax.set_title("Cubes", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Cube of Value", fontsize=14)
+# set the size of tick labels.
+ax.tick_params(labelsize=14)
+# Set the range of each axis.
+ax.axis((0, 5_100, 0, 132651000000 ))
+ax.ticklabel_format(style='plain')
+# Set t
+
+plt.show()
