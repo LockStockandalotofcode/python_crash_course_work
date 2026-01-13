f03799932ec9f0f62e7f8a4e60e40c229237770d
LockStockandalotofcode <shivanie0101@gmail.com>
2026-01-13 19:50:42 +0530

    refactor: optimize dice simulation algorithms using vectorized list comprehensions and generator expressions

---
+++ ch_15_generating_data/exercises/ex_09/dice_visual.py
@@ -0,0 +1,39 @@
+import plotly.express as px
+
+from die import Die
+
+# Create Two D6 dice.
+die_1 = Die()
+die_2 = Die()
+
+# Make some rolls, and store results in a list.
+results = []
+results = [(die_1.roll() + die_2.roll()) for roll_num in range(1000)]
+# for roll_num in range(1000):
+#     result = die_1.roll() + die_2.roll()
+#     results.append(result)
+
+# Analyze the results.
+frequencies = []
+max_result = die_1.num_sides + die_2.num_sides
+poss_results = range(2, max_result+1)
+frequencies = [results.count(value) for value in poss_results]
+# for value in poss_results:
+#     frequency = results.count(value)
+#     frequencies.append(frequency)
+
+# print(results)
+# print(frequencies)
+
+# Visualise the results.
+title = "Results of Rolling Two D6 1,000 times"
+labels = {
+    'x': 'Result',
+    'y': 'Frequency of Result'
+    }
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+
+# Further customize the chart.
+fig.update_layout(xaxis_dtick=1)
+
+fig.show()
