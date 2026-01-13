da9459c0bf7101498c1421a4f5309a8d92b330f5
LockStockandalotofcode <shivanie0101@gmail.com>
2026-01-14 02:28:23 +0530

    feat: Data Visualization project ch 16 ex-8: create visualization of recent seismic activity using live USGS earthquake feeds

---
+++ ch_16_downloading_data/exercises/ex_08/recent_earthquakes.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+import json
+
+import plotly.express as px
+
+# Read data as a string and convert to a python object.
+path = Path('eq_data/all_week.geojson')
+contents = path.read_text()
+all_eq_data = json.loads(contents)
+
+# # Create a more readable version of the data file.
+# path = Path('eq_data/readable_eq_data.geojson')
+# readable_contents = json.dumps(all_eq_data, indent=4)
+# path.write_text(readable_contents)
+
+# Examine all the earthquakes in the dataset.
+all_eq_dicts = all_eq_data['features']
+# print(len(all_eq_dicts))
+
+mags, lons, lats, eq_titles = [], [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    if mag == None or mag < 0: # to handle mag values given -ve values 
+        continue
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    eq_title = eq_dict['properties']['title']
+
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+    eq_titles.append(eq_title)
+
+# print(mags[:10])
+# print(lons[:5])
+# print(lats[:5])
+
+title = all_eq_data['metadata']['title']
+fig = px.scatter_geo(lat=lats, lon=lons, size=mags, title=title,
+        color=mags,
+        color_continuous_scale='bluered', # bluered, icefire, tealrose, 
+        labels={'color':"Magnitude"},
+        projection='natural earth',
+        # hover_name=eq_titles, 
+    )
+fig.show()
